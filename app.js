1ce123220202ccf00702d71223c569aff6786a93
jonathanh <jonforest@gmail.com>
2017-09-07 07:10:50 +1200

    Added endpoints to allow the adding and removing of users from the allowed-users list on the firebase db

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 const express = require('express')
 const app = express()
-var admin = require("firebase-admin");
+const admin = require("firebase-admin");
 const port = process.env.PORT || 3000
-const sakLocation = process.env.SAK_LOCATION || '/tmp/serviceacccountkey.json'
-const projectName = process.env.PROJECT_NAME
+const sakLocation = process.env.SAK_LOCATION || '/tmp/serviceaccountkey.json'
+const projectName = process.env.PROJECT_NAME 
 
 
 const serviceAccount = require(sakLocation);
@@ -31,9 +31,39 @@ app.get('/verify-id', function (req, res) {
     res.json(decodedToken)
   }).catch(function(error) {
     res.status(404).send('Token not found or not valid')
-  });
+  })
 })
 
+// These methods are specific to implementation, but can be used as examples on to update the database
+app.post('/allowed-user/:email', function (req, res) {
+  let email = req.params.email;
+  if (!email) {
+    res.status(400).send('No email provided')
+    return
+  }
+
+  // Firebase doesn't allow periods in it's keys, so replace any periods with commas
+  email = email.replace(/\./g, ',')
+
+  admin.database().ref(`allowed-users/${email}`).set(true)
+    .then(() => res.status(201).send('User added'))
+    .catch((err) => res.status(500).send(`Error adding user - ${err}`))
+})
+
+app.delete('/allowed-user/:email', function (req, res) {
+  let email = req.params.email
+  if (!email) {
+    res.status(400).send('No email provided')
+    return
+  }
+
+  // Firebase doesn't allow periods in it's keys, so replace any periods with commas
+  email = email.replace(/\./g, ',')
+
+  admin.database().ref(`allowed-users/${email}`).remove()
+    .then(() => res.status(204).send())
+    .catch((err) => res.status(500).send('Failed to add user'))
+})
 
 app.listen(port, function () {
   console.log(`Listening on port: ${port}`)
